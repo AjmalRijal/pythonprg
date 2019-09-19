4ac19a369b914a3d60a44f91fc19537dfb5de2d1
AjmalRijal <ajmal14449k@gmail.com>
2019-09-19 06:23:28 +0530

    smallest

---
+++ smallest.py
@@ -0,0 +1,9 @@
+x=int(input("Enter The First Number"));
+y=int(input("Enter The Second Number"));
+if(x<y):
+
+    print ("The Number X Is Smaller");
+
+else:
+
+    print ("The Number Y Is Smaller");
