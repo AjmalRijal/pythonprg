56453d4039879a15718665ae7fd6e5530353e96c
AjmalRijal <ajmal14449k@gmail.com>
2019-09-19 06:17:37 +0530

    largest

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
 x=int(input("Enter The First Number"));
-y=int(input("Enter The Second Number"));
+y=int(input("Enter The Second Num,ber"));
 z=x+y;
 print (z)
---
+++ largest.py
@@ -0,0 +1,9 @@
+x=int(input("Enter The First Number"));
+y=int(input("Enter The Second Number"));
+if(x>y):
+
+    print ("The Number X Is Larger");
+
+else:
+
+    print ("The Number Y Is Larger");
