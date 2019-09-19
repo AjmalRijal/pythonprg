443208f83440764329ea81f2c87fcc57a4ceb373
AjmalRijal <ajmal14449k@gmail.com>
2019-09-19 14:26:29 +0530

    temp

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template  
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-        return "<i>Welcome To My Web-Site</i>"
+        return render_template("index.html")
 
 @app.route("/home")       
 def home():
-        return "<i>Welcome To My Home Page<i>"
-        
+        return render_template("index2.html")
 if(__name__=="__main__"):
-    app.run()
+    app.run(debug=True)
